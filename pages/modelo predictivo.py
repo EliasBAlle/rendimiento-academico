0728a566cda8781bae5ed8225263722ee6c7d5d0
EliasBAlle <eliasballe0@gmail.com>
2024-06-27 15:00:10 -0500

    actualizacion nueva

--- pages/modelo predictivo.py
+++ pages/modelo predictivo.py
@@ -3,17 +3,35 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-
-# Título de la aplicación
-st.title('Predictor de Rendimiento Académico')
-
 # Cargar los datos
-data = pd.read_excel('Data/data_academic_performance.xlsx')
+df = pd.read_excel('../Data/data_academic_performance.csv')
+# Mapeo de valores textuales a numéricos
+map_values = {
+    'Three': 3,
+    'Five': 5,
+    'One': 1,
+    'Four': 4,
+    'Six': 6,
+    'Two': 2,
+    'Twelve or more': 12,
+    'Nueve': 9,
+    'Eight': 8,
+    'Seven': 7,
+    'Ten': 10,
+    'Once': 11,
+    0: 0
+}
+
+# Aplicar el mapeo a la columna
+df['PEOPLE_HOUSE'] = df['PEOPLE_HOUSE'].map(map_values)
+
+# Verificar que todos los valores ahora son numéricos
+print(df['PEOPLE_HOUSE'].unique())
 
 # Seleccionar las características y la etiqueta
 features = ['GENDER', 'EDU_FATHER', 'EDU_MOTHER', 'STRATUM', 'PEOPLE_HOUSE', 'CC_PRO', 'ENG_PRO', 'WC_PRO', 'FEP_PRO', 'G_SC']
-X = data[features]
-y = data['PERCENTILE']
+X = df[features]
+y = df['PERCENTILE']
 
 # Codificar variables categóricas
 X = pd.get_dummies(X, drop_first=True)
@@ -34,16 +52,16 @@ r2 = r2_score(y_test, y_pred)
 
 # Crear la interfaz de Streamlit
 st.sidebar.subheader('Parámetros de Entrada')
-gender = st.sidebar.selectbox('Género', data['GENDER'].unique())
-edu_father = st.sidebar.selectbox('Educación del Padre', data['EDU_FATHER'].unique())
-edu_mother = st.sidebar.selectbox('Educación de la Madre', data['EDU_MOTHER'].unique())
-stratum = st.sidebar.selectbox('Estrato', data['STRATUM'].unique())
-people_house = st.sidebar.slider('Número de Personas en la Casa', int(data['PEOPLE_HOUSE'].min()), int(data['PEOPLE_HOUSE'].max()), int(data['PEOPLE_HOUSE'].mean()))
-cc_pro = st.sidebar.slider('CC_PRO', int(data['CC_PRO'].min()), int(data['CC_PRO'].max()), int(data['CC_PRO'].mean()))
-eng_pro = st.sidebar.slider('ENG_PRO', int(data['ENG_PRO'].min()), int(data['ENG_PRO'].max()), int(data['ENG_PRO'].mean()))
-wc_pro = st.sidebar.slider('WC_PRO', int(data['WC_PRO'].min()), int(data['WC_PRO'].max()), int(data['WC_PRO'].mean()))
-fep_pro = st.sidebar.slider('FEP_PRO', int(data['FEP_PRO'].min()), int(data['FEP_PRO'].max()), int(data['FEP_PRO'].mean()))
-g_sc = st.sidebar.slider('G_SC', int(data['G_SC'].min()), int(data['G_SC'].max()), int(data['G_SC'].mean()))
+gender = st.sidebar.selectbox('Género', df['GENDER'].unique())
+edu_father = st.sidebar.selectbox('Educación del Padre', df['EDU_FATHER'].unique())
+edu_mother = st.sidebar.selectbox('Educación de la Madre', df['EDU_MOTHER'].unique())
+stratum = st.sidebar.selectbox('Estrato', df['STRATUM'].unique())
+people_house = st.sidebar.slider('Número de Personas en la Casa', int(df['PEOPLE_HOUSE'].min()), int(df['PEOPLE_HOUSE'].max()), int(df['PEOPLE_HOUSE'].mean()))
+cc_pro = st.sidebar.slider('CC_PRO', int(df['CC_PRO'].min()), int(df['CC_PRO'].max()), int(df['CC_PRO'].mean()))
+eng_pro = st.sidebar.slider('ENG_PRO', int(df['ENG_PRO'].min()), int(df['ENG_PRO'].max()), int(df['ENG_PRO'].mean()))
+wc_pro = st.sidebar.slider('WC_PRO', int(df['WC_PRO'].min()), int(df['WC_PRO'].max()), int(df['WC_PRO'].mean()))
+fep_pro = st.sidebar.slider('FEP_PRO', int(df['FEP_PRO'].min()), int(df['FEP_PRO'].max()), int(df['FEP_PRO'].mean()))
+g_sc = st.sidebar.slider('G_SC', int(df['G_SC'].min()), int(df['G_SC'].max()), int(df['G_SC'].mean()))
 
 # Crear entrada del usuario
 entrada = pd.DataFrame([[gender, edu_father, edu_mother, stratum, people_house, cc_pro, eng_pro, wc_pro, fep_pro, g_sc]], columns=features)
